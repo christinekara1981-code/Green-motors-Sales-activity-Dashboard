8e9781f57df12a28ecf830bdd23d7578c99a7be5
christinekara1981-code <christinekara1981@gmail.com>
2026-07-01 13:28:28 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // Paste your deployed Apps Script Web App URL here. It must end with /exec.
-  API_URL: "https://script.google.com/macros/s/YOUR-LATEST-DEPLOYMENT-ID/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwFjE92EIq8huDdQkRu8GgJINDu02kKpCLhaO7NoBP36c6xBLa6QXLi2bVJF-babL6v/exec"
 };
